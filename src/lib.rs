4feb2e6f43387aaf79cc283fe78ce85de0bed65b
Takumi Kanada <candy12t@gmail.com>
2021-02-27 16:38:47 +0900

    add: apply_some_filter

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,36 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate photon_rs;
+extern crate rand;
+extern crate wasm_bindgen;
+
+use photon_rs::{filters::filter, PhotonImage};
+use rand::seq::SliceRandom;
+use wasm_bindgen::prelude::wasm_bindgen;
+
+// ブラウザー・Node.js共通のコード
+const FILTERS: &[&str] = &[
+    "oceanic",
+    "islands",
+    "marine",
+    "seagreen",
+    "flagblue",
+    "liquid",
+    "diamante",
+    "radio",
+    "twenties",
+    "rosetint",
+    "mauve",
+    "bluechrome",
+    "vintage",
+    "perfume",
+    "serenity",
+];
+
+#[wasm_bindgen]
+pub fn apply_some_filter(mut img: &mut PhotonImage) -> String {
+    let mut rng = rand::thread_rng();
+    let chosen = FILTERS
+        .choose(&mut rng)
+        .expect("Can't choose a filter name!");
+    filter(&mut img, chosen);
+    chosen.to_string()
 }
